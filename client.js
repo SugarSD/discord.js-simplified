75f49a186d797f3d09cdb4efcabf05257c7124bc
SugarSD <jdrice5250@gmail.com>
2024-11-22 10:25:11 -0700

    Upd Docs, add functions ("setPrefix", "onCommand"), Clean Up Code

--- client.js
+++ client.js
@@ -1,27 +1,59 @@
 const { Client: RawClient, Events, GatewayIntentBits } = require('discord.js');
 
 class Client {
+
     constructor(token, intentsArray) {
         //Add "GatewayIntentBits" to the beginning of each intent in the "intentsArray"
         this.intents = intentsArray.map(intents => GatewayIntentBits[intents]);
         this.client = new RawClient({ intents: this.intents });
         this.token = token;
+        this.prefix = "";
+        this.commands = [];
+    }
+
+    setPrefix(prefix) {
+        //Sets the prefix for "onCommand()"
+
+        this.prefix = prefix;
     }
 
     on(event, callback) {
+        //Registers a new event
+
         this.client.on(Events[event], callback)
     }
 
     onCommand(command, callback) {
+        //Registers a new prefix command
 
+        this.commands.push({name: command, cb: callback});
+        //Push command to command array
     }
 
-    ready(listeners) {
-        //Wait for client to be ready and then run "client.on" listeners
-        this.client.once(Events.ClientReady, () => listeners());
+    ready(callback) {
+        //Once client is ready it will run this callback
+
+        this.client.once(Events.ClientReady, () => callback());
     }
 
     login() {
+        //Initialize discord bot
+
+        if(this.commands.length > 0) {
+            //Check if there's any commands in the commands array
+
+            this.prefix === "" ? console.warn("Bot will respond to commands with no prefix. Use 'setPrefix(prefix)'") : null;
+            //Check if prefix is set and warn if not
+
+            this.client.on(Events.MessageCreate, (message) => {
+                for (let i = 0; i < this.commands.length; i++) {
+                    if(message.content.startsWith(this.prefix + this.commands[i].name)) {
+                        this.commands[i].cb(message);
+                    }
+                }
+            })
+        }
+        //Login
         this.client.login(this.token);
     }
 }
